6de0d5ab9c084e6460fa12124de59c6495bc05a0
Elias Schmolke <elias.schmolke@icloud.com>
2026-09-19 18:51:23 +0200

    Initial Commit

---
+++ Huemeleon/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Huemeleon
+//
+//  Created by Elias Schmolke on 19.09.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HuemeleonTests/HuemeleonTests.swift
@@ -0,0 +1,17 @@
+//
+//  HuemeleonTests.swift
+//  HuemeleonTests
+//
+//  Created by Elias Schmolke on 19.09.26.
+//
+
+import Testing
+@testable import Huemeleon
+
+struct HuemeleonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
